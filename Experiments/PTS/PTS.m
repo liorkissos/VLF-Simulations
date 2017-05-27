27cbe4d36baad7acfe30c05236b5ca97209f7907
User <User@10.0.0.3>
2017-05-27 14:11:30 +0300

    1) corrected error: order of ifft and multiplication by vector b was incorrect 2) corrected error: measurment of power and of PAPR was incorrect

--- Experiments/PTS/PTS.m
+++ Experiments/PTS/PTS.m
@@ -4,102 +4,13 @@ clear
 close all
 
 
-% %%%%%%%%%%%%%%%%%%%%%%%%%%%
-% %%% Complex
-% %%%%%%%%%%%%%%%%%%%%%%%%%
-% 
-% M=4;
-% N=2^6;
-% 
-% W=4;
-% 
-% Modulation=4;
-% 
-% 
-% %% Data Generation
-% 
-% X_orig=randi(Modulation,N,1);
-% 
-% X_orig_shift=ifftshift(X_orig);
-% 
-% 
-% %% Phase optimization
-% 
-% phase=randi(W,M)-1;
-% b=exp(j*2*pi*phase/W);
-% 
-% % b=ones(M,1);
-% % 
-% % for kk=2:M
-% %     
-% %     for ll=0:W-1
-% %         
-% %     end
-% %     
-% % end
-% 
-% %% Tx
-% 
-% 
-% for kk=1:M
-%     ind_vec=kk:M:N;
-%     X_subs=X_orig_shift(ind_vec);
-%     X_orig_mat(:,kk)=b(kk)*upsample(X_subs,M,kk-1);
-%     
-% end
-% 
-% X_Tx_mat=X_orig_mat;
-% x_Tx_mat=ifft(X_Tx_mat);
-% 
-% x_Tx=sum(x_Tx_mat,2);
-% 
-% 
-% %% PAPR measurement
-% 
-% x_Tx_orig=ifft(X_orig_shift);
-% P_max_orig=max(abs(x_Tx_orig));
-% P_avg_orig=mean(abs(x_Tx_orig));
-% PAPR_orig=10*log10(P_max_orig/P_avg_orig)
-% 
-% P_max_manip=max(abs(x_Tx));
-% P_avg_manip=mean(abs(x_Tx));
-% PAPR_manip=10*log10(P_max_manip/P_avg_manip)
-% 
-% %% Rx
-% 
-% X_Rx=fft(x_Tx);
-% 
-% for kk=1:M
-%     ind_vec=kk:M:N;
-%     X_Rx_subs=X_Rx(ind_vec);
-%     X_Rx_mat(:,kk)=(1./b(kk))*upsample(X_Rx_subs,M,kk-1);
-%     
-% end
-% 
-% X_Rx=sum(X_Rx_mat,2);
-% 
-% X_Rx=fftshift(X_Rx);
-% 
-% error=max(abs(X_orig-X_Rx))
-% figure
-% set(gcf,'windowstyle','docked')
-% shg
-% stem(real(X_orig));hold on;stem(real(X_Rx))
-% title(['Error= ',num2str(error),''])
-% legend('original','reconstructed')
-% grid minor
 
-
-
-%%%%%%%%%%%%%%%%%%%%%%%%
-%%% Final
-%%%%%%%%%%%%%%%%%%%5
 
 %% User defined parameters
 
 %%% Standard
-N=2^4; % length of symbol
-Modulation=4; % modulation index
+N=2^6; % length of symbol
+Modulation=64; % modulation index
 
 hMod_data = comm.RectangularQAMModulator('ModulationOrder',Modulation,'NormalizationMethod','Average Power','BitInput',0);
 hDemod = comm.RectangularQAMDemodulator('ModulationOrder',Modulation,'NormalizationMethod','Average Power','BitOutput',0);
@@ -134,13 +45,13 @@ b=ones(M,1); % see page 2 bottom left
 
 for     kk=2:M
     
-    X_Tx_mat=X_subs_mat*diag(b); % like multiplying every column by the same integer
-    x_Tx_mat=ifft(X_Tx_mat);
+    x_subs_mat=ifft(X_subs_mat);
+    x_Tx_mat=x_subs_mat*diag(b); % like multiplying every column by the same integer
     x_Tx=sum(x_Tx_mat,2);
     
-    P_max=max(abs(x_Tx));
-    P_avg=mean(abs(x_Tx));
-    PAPR_ref=10*log10(P_max/P_avg)
+    P_max=max(x_Tx.*conj(x_Tx));
+    P_avg=mean(x_Tx.*conj(x_Tx));
+    PAPR_ref=10*log10(P_max/P_avg);
     PAPR_min=PAPR_ref;
     
     ll_opt=0;
@@ -151,8 +62,8 @@ for     kk=2:M
         x_Tx_mat=ifft(X_Tx_mat);
         x_Tx=sum(x_Tx_mat,2);
         
-        P_max=max(abs(x_Tx));
-        P_avg=mean(abs(x_Tx));
+        P_max=max(x_Tx.*conj(x_Tx));
+        P_avg=mean(x_Tx.*conj(x_Tx));
         PAPR=10*log10(P_max/P_avg);
         
         if PAPR<PAPR_min
@@ -164,8 +75,8 @@ for     kk=2:M
     
     b(kk)=exp(j*2*pi*ll_opt/W);
     
-    kk
-    PAPR_min
+    
+    PAPR_min_vec(kk)=PAPR_min;
     
 end
 
@@ -179,12 +90,12 @@ x_Tx=sum(x_Tx_mat,2);
 %% PAPR measurement
 
 x_Tx_orig=ifft(X_orig_shift);
-P_max_orig=max(abs(x_Tx_orig));
-P_avg_orig=mean(abs(x_Tx_orig));
+P_max_orig=max(x_Tx_orig.*conj(x_Tx_orig));
+P_avg_orig=mean(x_Tx_orig.*conj(x_Tx_orig));
 PAPR_orig=10*log10(P_max_orig/P_avg_orig)
 
-P_max_manip=max(abs(x_Tx));
-P_avg_manip=mean(abs(x_Tx));
+P_max_manip=max(x_Tx.*conj(x_Tx));
+P_avg_manip=mean(x_Tx.*conj(x_Tx));
 PAPR_manip=10*log10(P_max_manip/P_avg_manip)
 
 %% Rx
