ed6c0468c1a552de6e927e8190b4c2897b9d80b3
User <User@10.0.0.3>
2017-05-27 15:23:35 +0300

    added further corrections to the PAPR reduction procedure. added a curve describing the PAPR evolution added sanity checks for some of the user defined parameters

--- Experiments/PTS/PTS.m
+++ Experiments/PTS/PTS.m
@@ -3,22 +3,31 @@ clc
 clear
 close all
 
-
+debug=0;
 
 
 %% User defined parameters
 
 %%% Standard
-N=2^6; % length of symbol
-Modulation=64; % modulation index
+N=2^8; % length of symbol
+Modulation=16; % modulation index
 
 hMod_data = comm.RectangularQAMModulator('ModulationOrder',Modulation,'NormalizationMethod','Average Power','BitInput',0);
 hDemod = comm.RectangularQAMDemodulator('ModulationOrder',Modulation,'NormalizationMethod','Average Power','BitOutput',0);
 
 %%% PTS
-M=4; % number of blocks the symbol is divided into
-W=4; % number of possible phase
+M=32; % number of blocks the symbol is divided into
+W=8; % number of possible phase
 
+%% Sanity checks
+
+if mod(log2(M),1) || mod(log2(N),1)
+    error('M and N should be a power of 2')
+end
+
+if mod(N/M,1)
+    error('the symbol s subset lengths should be an integer number')
+end
 
 %% Data Generation
 
@@ -28,11 +37,18 @@ X_orig = step(hMod_data, data_Tx); % QAM modulation
 
 X_orig_shift=ifftshift(X_orig);
 
+%%
+x_Tx_orig=ifft(X_orig_shift);
+P_max_orig=max(x_Tx_orig.*conj(x_Tx_orig));
+P_avg_orig=mean(x_Tx_orig.*conj(x_Tx_orig));
+PAPR_orig=10*log10(P_max_orig/P_avg_orig);
+
 
 %% Tx Symbol splitting
 
 %%% splitting in a "polyphase style"
 
+X_subs_mat=zeros(N,M);
 for kk=1:M
     ind_vec=kk:M:N;
     X_subs_mat(:,kk)=upsample(X_orig_shift(ind_vec),M,kk-1);
@@ -41,11 +57,13 @@ end
 
 %% Phase optimization: see PAPR Reduction of OFDM Signals Using a Reduced Complexity PTS Technique, Hee Han& Hong Lee
 
-b=ones(M,1); % see page 2 bottom left
+PAPR_min_vec=zeros(M,1);
+PAPR_min_vec(1)=PAPR_orig;
 
+b=ones(M,1); % see page 2 bottom left
 for     kk=2:M
     
-    x_subs_mat=ifft(X_subs_mat);
+    x_subs_mat=ifft(X_subs_mat); % IDFT over the zero padded subsets of X
     x_Tx_mat=x_subs_mat*diag(b); % like multiplying every column by the same integer
     x_Tx=sum(x_Tx_mat,2);
     
@@ -55,11 +73,11 @@ for     kk=2:M
     PAPR_min=PAPR_ref;
     
     ll_opt=0;
-    for ll=0:W-1
+    for ll=0:W-1 % equation (4) in Han & Lee
         
         b(kk)=exp(j*2*pi*ll/W);
-        X_Tx_mat=X_subs_mat*diag(b);
-        x_Tx_mat=ifft(X_Tx_mat);
+        x_subs_mat=ifft(X_subs_mat);
+        x_Tx_mat=x_subs_mat*diag(b);
         x_Tx=sum(x_Tx_mat,2);
         
         P_max=max(x_Tx.*conj(x_Tx));
@@ -80,28 +98,28 @@ for     kk=2:M
     
 end
 
-X_Tx_mat=X_subs_mat*diag(b);
-
-x_Tx_mat=ifft(X_Tx_mat);
-
+x_subs_mat=ifft(X_subs_mat);
+x_Tx_mat=x_subs_mat*diag(b);
 x_Tx=sum(x_Tx_mat,2);
 
 
 %% PAPR measurement
 
-x_Tx_orig=ifft(X_orig_shift);
-P_max_orig=max(x_Tx_orig.*conj(x_Tx_orig));
-P_avg_orig=mean(x_Tx_orig.*conj(x_Tx_orig));
-PAPR_orig=10*log10(P_max_orig/P_avg_orig)
+% x_Tx_orig=ifft(X_orig_shift);
+% P_max_orig=max(x_Tx_orig.*conj(x_Tx_orig));
+% P_avg_orig=mean(x_Tx_orig.*conj(x_Tx_orig));
+% PAPR_orig=10*log10(P_max_orig/P_avg_orig)
 
 P_max_manip=max(x_Tx.*conj(x_Tx));
 P_avg_manip=mean(x_Tx.*conj(x_Tx));
-PAPR_manip=10*log10(P_max_manip/P_avg_manip)
+PAPR_manip=10*log10(P_max_manip/P_avg_manip);
+PAPR_reduction=PAPR_orig-PAPR_manip
 
 %% Rx
 
 X_Rx=fft(x_Tx);
 
+X_Rx_mat=zeros(N,M);
 for kk=1:M
     ind_vec=kk:M:N;
     X_Rx_subs=X_Rx(ind_vec);
@@ -119,12 +137,27 @@ data_Rx=step(hDemod,X_Rx);
 %% Analysis
 
 error=max(abs(data_Tx-data_Rx))
+
 figure
 set(gcf,'windowstyle','docked')
 shg
-stem(data_Tx);hold on;stem(data_Rx)
-title(['Error= ',num2str(error),''])
-legend('original','reconstructed')
+plot(PAPR_min_vec)
 grid minor
+title({['PAPR optimization evolution'],['PAPR reduction=',num2str(PAPR_reduction),'[dB]']})
+ylabel('PAPR [dB]')
+xlabel('iteration')
+
+
+if debug
+    
+    figure
+    set(gcf,'windowstyle','docked')
+    shg
+    stem(data_Tx);hold on;stem(data_Rx)
+    title(['Error= ',num2str(error),''])
+    legend('original','reconstructed')
+    grid minor
+    
+end
 
 
